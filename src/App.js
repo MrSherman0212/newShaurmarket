3919bcfaa569b4b5b3406b55a18d1722992c5b96
shergazy <taalaibek.uulu.shergazy@gmail.com>
2021-08-13 10:56:28 +0600

    sher's media.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Sidebar from './Container/Sidebar/Sidebar';
 import Routes from './Routes';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './Container/sherMedia.css'
 
 function App() {
   return (
